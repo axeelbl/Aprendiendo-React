6921a36dbfd1bce2c603673490ab622018f9e75c
axeelbl <axelbl54@gmail.com>
2024-07-11 13:59:34 +0200

    11/07/2024 - 13:59

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -3,24 +3,31 @@ import confetti from "canvas-confetti"
 import { Square } from "./components/Square.jsx"
 import { TURNS} from "./constants.js"
 import { checkWinner } from "./logic/board.js"
+import { WinnerModal } from "./components/WinnerModal.jsx"
+import { checkEndGame } from "./logic/board.js"
 
 function App() {
-  const [board,setBoard] = useState(Array(9).fill(null))
+  const [board,setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage): Array(9).fill(null)
+  })
 
-  const [turn,setTurn] = useState(TURNS.X)
+  const [turn,setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   const [winner, setWinner] = useState(null) // null es que no hay ganador y false es que hay empate
 
-  
 
   const resetGame = () =>{
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
-  }
 
-  const checkEndGame = (newBoard) =>{
-    return newBoard.every((square) => square != null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+    
   }
 
   const updateBoard = (index) => {
@@ -33,6 +40,13 @@ function App() {
     // cambiar el turno
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    // guardar aqui partida
+    window.localStorage.setItem('board',JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+
+
+    // revisar si hay ganador
     const newWinner = checkWinner(newBoard)
     if (newWinner){
       confetti()
@@ -74,29 +88,7 @@ function App() {
         </Square>
       </section>
 
-      {
-        winner != null && (
-          <section className="winner">
-            <div className="text">
-              <h2>
-                {
-                  winner == false
-                  ? 'Empate'
-                  : 'Ganó:' 
-                }
-              </h2>
-
-              <header className="win">
-                {winner && <Square>{winner}</Square>}
-              </header>
-
-              <footer>
-                <button onClick={resetGame}>Empezar de Nuevo</button>
-              </footer>
-            </div>
-          </section>
-        )
-      }
+      <WinnerModal resetGame={resetGame} winner={winner} />
 
 
 
--- projects/02-tic-tac-toe/src/logic/board.js
+++ projects/02-tic-tac-toe/src/logic/board.js
@@ -11,4 +11,9 @@ export const checkWinner = (boardToCheck) => {
       }
     }
     return null
-  }
+}
+
+export const checkEndGame = (newBoard) =>{
+  return newBoard.every((square) => square != null)
+}
+
